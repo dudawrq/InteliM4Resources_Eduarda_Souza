c210ce612b43df4cff2af6533e6a096944d74692
dudawrq <eduarda.souza@sou.inteli.edu.br>
2023-10-22 16:13:34 -0300

    entrega ponderada 1
    Código da resolução da ponderada de programação da semana 1

---
+++ SEMANA_01/01_CODIGOS/ponderada_1_programa_o1.ino
@@ -0,0 +1,23 @@
+// C++ code
+//
+void setup()
+{
+	pinMode(8, OUTPUT); //Estabelecendo qual pino será usado
+  	pinMode(5, OUTPUT); //Estabelecendo qual pino será usado
+  	pinMode(2, OUTPUT); //Estabelecendo qual pino será usado
+  
+
+}
+
+void loop()
+{
+  digitalWrite(8, HIGH); //Ligando LED da porta 9
+  delay(200); //Tempo de espera
+  digitalWrite(8, LOW); //Desligando LED da porta 9
+  digitalWrite(5, HIGH); //Ligando LED da porta 5
+  delay(200); //Tempo de espera
+  digitalWrite(5, LOW); //Desligando LED da porta 5
+  digitalWrite(2, HIGH); //Ligando LED da porta 0
+  delay(200); //Tempo de espera
+  digitalWrite(2, LOW); //Desligando LED da porta 0
+}
